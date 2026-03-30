d3fce6286c21ed64c6d9318e3d74f49621106773
sankhyapriyo Dey <88035368+sankhya007@users.noreply.github.com>
2026-03-30 23:01:05 +0530

    Add: concatenation-of-array - submission-1

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def getConcatenation(self, nums): 
+        return nums + nums
+        print(result) 
+
+sol = Solution()
+
+array = [1,2,3,4,5,6,7]
+print(sol.getConcatenation(array))
+
+        
